b03b58738366c53af9c198f033733d563e77ee9f
João Lucas Bernardi <joao_bernardi@outlook.com>
2023-01-24 15:41:28 -0300

    `Svelte` - Add homepage

--- project/home/svelte/src/routes/+page.svelte
+++ project/home/svelte/src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { Instagram, Twitter, Github } from "../lib/components";
+    import { fade } from 'svelte/transition';
+    import { onMount } from 'svelte';
+    let visible = false;
+    onMount(async () => {
+		setTimeout(() => {visible = true;}, 500)
+	});
+</script>
+
+<svelte:head>
+    <title>Homepage | jaobernardi.com</title>
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="@jaobernard" />
+    <meta name="twitter:creator" content="@jaobernard" />
+    <meta property="og:url" content="https://jaobernardi.com" />
+    <meta property="og:title" content="Home" />
+    <meta property="og:description" content="Oi, eu sou o jão :)" />
+    <meta property="og:image" content="https://static.jaobernardi.com/img/favicon/fullsize.png" />
+
+</svelte:head>
+
+{#if visible}  
+    <div class="display-container" transition:fade>
+        <h1>Oi, eu sou o jão :)</h1>
+        <pre>(@jaobernardi)</pre>
+        <p>Bem-vindo à minha página na <span class="emphasis-box times">internet</span></p>
+        <sub>(。・∀・)ノ</sub>
+        <div class="flex">
+            <Instagram handle="jaobernardi"/>
+            <Twitter handle="jaobernard"/>
+            <Github handle="jaobernardi"/>
+        </div>
+    </div>
+{/if}
--- project/home/svelte/src/routes/+page.ts
+++ project/home/svelte/src/routes/+page.ts
@@ -1,6 +1,4 @@
 // @ts-nocheck
 import { error } from '@sveltejs/kit';
 
-export function load({ params }) {
-    throw error(403, "Forbidden");
-}
+export function load({ params }) {}
